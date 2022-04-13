5cf904fdb809cceb1a9a0dbb7010bf758db1c023
asdi80 <a8sadi@gmail.com>
2022-04-12 23:46:25 -0700

    updating

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/maintainable1', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/maintainable', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
